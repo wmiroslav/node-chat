3c91ff5d8a7158198d8a333e7283349ebe505798
wmiroslav <vmiroslav88@gmail.com>
2018-04-16 15:01:49 +0200

    changes

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 // require('./server/server');
-console.log(111);
+
+
+
+const path = require('path');
+const express = require('express');
+const app = express();
+
+const publicPath = path.join(__dirname, '..', 'public')
+app.use(express.static(publicPath));
+                                    
+                                    
+
+const port = process.env.port || 3000;
+app.listen(port, ()=>{
+    console.log('Listening on ' + port);
+})
+
+
+
+
