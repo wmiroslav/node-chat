d9edb0423d294be38bbfc9ea4e1ef4dcaa014de8
wmiroslav <vmiroslav88@gmail.com>
2018-04-16 14:36:19 +0200

    changes

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public')
-app.use(express.static(publicPath));
+const publicPath = path.join(__dirname, 'public')
+app.use(express.static(publicPath)); 
                                     
                                     
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
     console.log('Listening on ' + port);
 })
