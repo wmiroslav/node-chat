9efe1f8fa760445f27875debfe919cb94d8d53a6
wmiroslav <vmiroslav88@gmail.com>
2018-04-16 15:02:59 +0200

    changes

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 
-const publicPath = path.join(__dirname, '..', 'public')
+const publicPath = path.join(__dirname, 'public')
 app.use(express.static(publicPath));
                                     
                                     
